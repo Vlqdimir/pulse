45c335de113f00249885bf4fb263edc21bd45ad7
Volodya <fedordo513@gmail.com>
2024-06-05 09:50:54 +0300

    tab

--- src/js/script.js
+++ src/js/script.js
@@ -1,66 +1,24 @@
-var name = "Vova";
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_arrows.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_arrows.svg"></button>',
+        resposive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
 
-letnumber = 7;
-const pi = 3.14;
-
-number = 4;
-
-let leftBorderWidth = 200;
-
-// number
-// string - '',"",``
-// true/false
-// null
-// undefined
-//let obj = {
-//    name: 'apple',
-//    color: 'green',
-//    weight: 200
-//}
-//Symbol
-
-//alert('fegege')
-//console.log(number);
-//let answer = confirm("Вам есть 18?");
-//console.log(answer)
-//let answer = prompt("Вам есть 18?", "");
-//console.log(answer)
-//console.log(4+5)
-//let isChecked = true,
-//    isClose = true;
-//   console.log(isChecked && isClose)
-//console.log(isChecked || isClose);
-//if (2*4 == 8*1) {
-//    console.log('Kfрно')
-//} else {
-//    console.log('Ошибка')
-//}
-
-//let answer = confirm("Вам есть 18?");
-//if (answer) {
-//    console.log('Проходите')
-//} else {
-//   console.log('Уходи') 
-//}
-
-//const num = 50;
-
-//if (num<49) {
-//    console.log('Неправильно')
-//} else if (num>100) {
-//    console.log('Много')
-//} else {
-//    console.log('Верно')
-//}
-
-//for(let i = 1; i < 8; i ++) {
-//    console.log(i)
-//}
-function logging(a, b) {
-    console.log(a + b)
-}
-
-logging(3, 5);
-logging(3, 12);
-
-
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
+});
